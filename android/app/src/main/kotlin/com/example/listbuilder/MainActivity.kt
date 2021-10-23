c8c63062b6f9d74d475b3c5e7da14d9361a8291b
kawsar07 <kawsar.ali.cse@gmail.com>
2021-10-24 01:02:44 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/listbuilder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.listbuilder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('List Builder'),
+        ),
+        body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (
+          context, morning
+          ){
+            return Container(
+child: Card(
+  child: ListTile(
+    leading: Text('Hellow'),
+  ),
+),
+            );
+          },
+
+        ),
+      ),
+    );
+  }
+}
+
